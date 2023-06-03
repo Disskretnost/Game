106aea080a84a028843b1f36cf51095a4e1ef65a
Ragech <nik.gasilin@mail.ru>
2023-06-03 19:58:44 +0300

    Камера

---
+++ Милюков/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public float mouseSensitivity = 10;
+    public Transform target;
+    public float dstFromTarget = 2;
+    public Vector2 pitchMinMax = new Vector2(-40, 85);
+
+    public float rotationSmoothTime = .12f;
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
+
+    float yaw;
+    float pitch;
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += Input.GetAxis("Mouse X");   
+        pitch -= Input.GetAxis("Mouse Y");
+        pitch = Mathf.Clamp(pitch,pitchMinMax.x,pitchMinMax.y);
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+        transform.eulerAngles = currentRotation;
+
+        transform.position = target.position - transform.forward * dstFromTarget;
+    }
+}
